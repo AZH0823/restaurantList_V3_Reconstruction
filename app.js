16d1619a6c25f6cff97663ac6d44d629bda9d1e4
AZH0823 <qoo9986360@gmail.com>
2021-12-15 00:21:12 +0800

    add sort Function & add enName on hbs

--- app.js
+++ app.js
@@ -36,8 +36,10 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 //index 主畫面的取得
 app.get('/', (req, res) => {
+
   return resList.find()
     .lean()
+    .sort({ name_en: 'asc' }) //預設a-z順序
     .then(restaurants => res.render('index', { resList: restaurants }))
     .catch(error => console.error(error))
 })
@@ -62,6 +64,7 @@ app.get('/restaurant/new', (req, res) => {
 
 //新增一筆資料
 app.post("/restaurant", (req, res) => {
+  console.log(req.body)
   return resList.create(req.body)
     .then(() => res.redirect("/"))
     .catch(error => console.log(error))
@@ -72,19 +75,38 @@ app.post("/restaurant", (req, res) => {
 app.get('/search', (req, res) => {
   // 與 index.handlebars name="keyword"
   let keyWord = req.query.keyword.trim().toLowerCase() // 存所查詢的關鍵字
-  if (!keyWord) {
-    res.redirect('/')
-    return
+  //先處理排序
+  let sort = req.query.sort
+  let sortMethod = {}
+  // console.log(sort)
+  switch (sort) {
+    case 'A > Z':
+      sortMethod.name_en = 'asc'
+      break
+    case 'Z > A':
+      sortMethod.name_en = 'desc'
+      break
+    case '餐廳類型':
+      sortMethod.category = 'asc'
+      break
+    case '地區':
+      sortMethod.location = 'asc'
+      break
+    default:
+      break
   }
+  // console.log(sortMethod)
+
 
   return resList.find()
     .lean()
+    .sort(sortMethod)
     .then((restaurants) => {
       const filiterResList = restaurants.filter((restaurant) => {
         // 存放符合關鍵字的餐廳清單。清單並不會只有1間; 所以使用filter
         return restaurant.name.trim().toLowerCase().includes(keyWord) || restaurant.category.trim().toLowerCase().includes(keyWord)
       })
-      res.render('index', { searchKeyWord: keyWord, resList: filiterResList })
+      res.render('index', { searchKeyWord: keyWord, resList: filiterResList, sort })
     })
     .catch(error => console.error(error))
 
@@ -97,7 +119,7 @@ app.get('/restaurant/:id/edit', (req, res) => {
   return resList.findById(id)
     .lean()
     .then(findOneRes => {
-      console.log(findOneRes.category)
+      // console.log(findOneRes.category)
       res.render('edit', { findOneRes })
       // console.log(findOneRes)
     })
@@ -109,11 +131,12 @@ app.get('/restaurant/:id/edit', (req, res) => {
 app.post('/restaurant/:id/', (req, res) => {
   const id = req.params.id
   const edit = req.body
-  console.log(edit.category)
+  // console.log(edit.category)
   return resList.findById(id)
     .then(editOneRestaurant => {
 
       editOneRestaurant.name = edit.name
+      editOneRestaurant.name_en = edit.name_en
       editOneRestaurant.category = edit.category
       editOneRestaurant.image = edit.image
       editOneRestaurant.rating = edit.rating
@@ -148,7 +171,7 @@ app.post('/restaurant/:id/delete', (req, res) => {
   return resList.findById(id)
     .then(removeRes => {
       if (typeof (removeRes) == null || typeof (removeRes) == undefined) {
-        console.log('無此筆資料')
+        // console.log('無此筆資料')
         return
       }
       removeRes.remove()
--- models/restaurantList_Todo.js
+++ models/restaurantList_Todo.js
@@ -32,7 +32,12 @@ const restaurantListSchema = new Schema({
   location: {
     type: String, // 資料型別是字串
     required: true // 這是個必填欄位
-  }
+  },
+  name_en: {
+    type: String,
+    required: true // 這是個必填欄位
+  },
+
 
 })
 module.exports = mongoose.model('restaurantLists', restaurantListSchema)
